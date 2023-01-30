3c07007bcaa2e1a30ef07eb314e9c395ec111056
NohEunSeok <eodyddnjs@naver.com>
2023-01-30 20:44:29 +0900

    * 세 수, 수 정렬하기2 문제 풀이

---
+++ src/코딩테스트/정렬/세수/Main.java
@@ -0,0 +1,79 @@
+package 코딩테스트.정렬.세수;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/10817 성공!
+ */
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] nums;
+
+    static void input() {
+        nums = new int[4];
+        for(int i = 1; i <= 3; i++){
+            nums[i] = scan.nextInt();
+        }
+    }
+
+    static void func(){
+        Arrays.sort(nums, 1, 4);
+        System.out.println(nums[2]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        func();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
