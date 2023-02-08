9722ced4dedc310dc4fe802bf2ad654ca9583440
NohEunSeok <esnoh@softnet.co.kr>
2023-02-08 16:35:10 +0900

    가장 긴 증가하는 부분수열3 문제풀이

--- src/코딩테스트/이분탐색/가장긴증가하는부분수열3/Main.java
+++ src/코딩테스트/이분탐색/가장긴증가하는부분수열3/Main.java
@@ -1,18 +1,21 @@
 package 코딩테스트.이분탐색.가장긴증가하는부분수열3;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
- * https://www.acmicpc.net/problem/12738
+ * https://www.acmicpc.net/problem/12738 다시풀기
  */
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, ans;
+    static int N;
     static int[] nums;
+    static List<Integer> list = new ArrayList<>();
 
     static void input() {
         N = scan.nextInt();
@@ -20,49 +23,35 @@ public class Main {
         for(int i = 1; i <= N; i++){
             nums[i] = scan.nextInt();
         }
-
-        Arrays.sort(nums, 1, N + 1);
-    }
-
-    static int upperBound(int a){
-        int L = 1, R = N, retVal = 0;
-        while(L <= R){
-            int mid = (L + R) / 2;
-
-            if(a > nums[mid]){
-                L = mid + 1;
-            } else {
-                R = mid - 1;
-                retVal = mid;
-                break;
-            }
-        }
-        return retVal;
-    }
-
-    static int bottomBound(int a){
-        int L = 1, R = N, retVal = 0;
-        while(L <= R){
-            int mid = (L + R) / 2;
-
-            if(a >= nums[mid]){
-                L = mid + 1;
-                retVal = mid;
-            } else {
-                R = mid - 1;
-            }
-        }
-        return retVal;
     }
 
     static void func(){
-        ans = upperBound(nums[N]) - bottomBound(nums[1]);
+        list.add(Integer.MIN_VALUE);
+
+        for(int i = 1; i <= N; i++){
+            int val = nums[i];
+
+            if(val > list.get(list.size() - 1)){
+                list.add(val);
+            }else {
+                int L = 1, R = list.size() - 1;
+                while(L < R){
+                    int mid = (L+R) / 2;
+                    if(list.get(mid) >= val){
+                        R = mid;
+                    } else {
+                        L = mid + 1;
+                    }
+                }
+                list.set(R, val);
+            }
+        }
+        System.out.println(list.size() - 1);
     }
 
     public static void main(String[] args) {
         input();
         func();
-        System.out.println(ans);
     }
 
 
