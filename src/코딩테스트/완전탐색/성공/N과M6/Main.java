453e4bb259f2c0d0f6d26017db68fa5c86167fa4
diiiiiiiino <eodyddnjs@naver.com>
2023-02-21 11:08:09 +0900

    알고리즘 패키지 리팩토링

---
+++ src/코딩테스트/완전탐색/성공/N과M6/Main.java
@@ -0,0 +1,96 @@
+package 코딩테스트.완전탐색.성공.N과M6;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/15655 성공!
+ */
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] nums, selected;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        nums = new int[N + 1];
+        for(int i = 1; i <= N; i++){
+            nums[i] = scan.nextInt();
+        }
+        selected = new int[M + 1];
+
+    }
+
+    static void rec_func(int k, int start) {
+        if(M + 1 == k){
+            for(int i = 1; i <= M; i++){
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for(int cand = start; cand <= N; cand++){
+                selected[k] = nums[cand];
+                rec_func(k + 1, cand + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        Arrays.sort(nums, 1, N + 1);
+        rec_func(1, 1);
+        System.out.println(sb.toString());
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ src/코딩테스트/이분탐색/다시풀기/휴게소세우기/Main.java
@@ -0,0 +1,106 @@
+package 코딩테스트.이분탐색.다시풀기.휴게소세우기;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1477 다시풀기!!
+ */
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M, L, ans;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        L = scan.nextInt();
+
+        A = new int[N + 2];
+        for(int i = 1; i <= N; i++){
+            A[i] = scan.nextInt();
+        }
+        A[N + 1] = L;
+
+        Arrays.sort(A, 1, N + 2);
+    }
+
+    static boolean search(int mid){
+        int count = 0;
+        for(int i = 1; i < N + 2; i++){
+            int diff = A[i] - A[i - 1] - 1;
+            count += (diff / mid);
+        }
+
+        return count <= M;
+    }
+
+    static void pro() {
+        int l = 1, r = L;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(search(mid)){
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
