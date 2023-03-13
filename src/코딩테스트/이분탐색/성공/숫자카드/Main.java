c0c09d0a31e98de63352c0cfd569fc4669432a0f
NohEunSeok <eodyddnjs@naver.com>
2023-03-13 21:44:19 +0900

    * 숫자카드 문제풀이

--- src/코딩테스트/이분탐색/성공/숫자카드/Main.java
+++ src/코딩테스트/이분탐색/성공/숫자카드/Main.java
@@ -1,4 +1,4 @@
-package 코딩테스트.이분탐색.숫자카드;
+package 코딩테스트.이분탐색.성공.숫자카드;
 
 import java.io.*;
 import java.util.Arrays;
@@ -11,13 +11,50 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static int[] A;
+    static int N, M;
+    static int[] A, B, ans;
 
     static void input() {
+        N = scan.nextInt();
+        A = new int[N + 1];
+        for(int i = 1; i <= N; i++){
+            A[i] = scan.nextInt();
+        }
+
+        M = scan.nextInt();
+        B = new int[M + 1];
+        ans = new int[M + 1];
+        for(int i = 1; i <= M; i++){
+            B[i] = scan.nextInt();
+        }
+
+        Arrays.sort(A, 1, N + 1);
+    }
+
+    static void search(int num, int i){
+        int L = 1, R = N;
+        while(L <= R){
+            int mid = (L + R) / 2;
+
+            if(A[mid] < num){
+                L = mid + 1;
+            } else if(A[mid] > num){
+                R = mid - 1;
+            } else {
+                ans[i] = 1;
+                break;
+            }
+        }
     }
 
     static void pro() {
+        for(int i = 1; i <= M; i++){
+            search(B[i], i);
+        }
+
+        for(int i = 1; i <= M; i++){
+            System.out.print(ans[i] + " ");
+        }
     }
 
     public static void main(String[] args) {
