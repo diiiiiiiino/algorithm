f577b3e4428938cd5e1e67e50cf1fe04f043583c
diiiiiiiino <eodyddnjs@naver.com>
2023-03-23 17:24:11 +0900

    토마토 (다시풀기)

--- src/코딩테스트/그래프/토마토/Main.java
+++ src/코딩테스트/그래프/토마토/Main.java
@@ -1,10 +1,12 @@
 package 코딩테스트.그래프.토마토;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * https://www.acmicpc.net/problem/
+ * https://www.acmicpc.net/problem/7569 다시풀기!
  */
 public class Main {
     static FastReader scan = new FastReader();
@@ -12,7 +14,7 @@ public class Main {
 
     static int M, N, H, ans;
     static int[][] nums;
-    static int[][][] maps;
+    static int[][][] maps, dist;
     static boolean[][][] visited;
     static int[][] dir = { { 0, -1, 0 } , { 0, 0, 1 }, { 0, 1, 0 }, { 0, 0, -1 }, { -1, 0, 0 }, { 1, 0, 0 } };
     static void input() {
@@ -23,22 +25,75 @@ public class Main {
         nums = new int[N][M];
         maps = new int[H][N][M];
         visited = new boolean[H][N][M];
+        dist = new int[H][N][M];
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                nums[i][j] = scan.nextInt();
+        for(int z = 0; z < H; z++){
+            for(int x = 0; x < N; x++){
+                for(int y = 0; y < M; y++){
+                    maps[z][x][y] = scan.nextInt();
+                }
+            }
+        }
+    }
+
+    static void bfs(){
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int z = 0; z < H; z++){
+            for(int x = 0; x < N; x++){
+                for(int y = 0; y < M; y++){
+                    dist[z][x][y] = -1;
+                    if(maps[z][x][y] == 1){
+                        dist[z][x][y] = 0;
+                        queue.add(z);
+                        queue.add(x);
+                        queue.add(y);
+                    }
+                }
             }
         }
 
-        for(int i = 0; i < H; i++){
-            maps[i] = nums;
+        while(!queue.isEmpty()){
+            int z = queue.poll();
+            int x = queue.poll();
+            int y = queue.poll();
+
+            for(int i = 0; i < 6; i++){
+                int nz = dir[i][0] + z;
+                int nx = dir[i][1] + x;
+                int ny = dir[i][2] + y;
+
+                if(nz < 0 || nx < 0 || ny < 0 || nz >= H || nx >= N || ny >= M) continue;
+                if(maps[nz][nx][ny] == -1) continue;
+                if(dist[nz][nx][ny] != -1) continue;
+
+                queue.add(nz);
+                queue.add(nx);
+                queue.add(ny);
+
+                dist[nz][nx][ny] = dist[z][x][y] + 1;
+            }
         }
-        
     }
 
     public static void main(String[] args) {
         input();
+        bfs();
 
+        int ans = 0;
+        for (int h = 0; h < H; h++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (maps[h][i][j] == -1) continue;
+                    if (dist[h][i][j] == -1) {
+                        System.out.println(-1);
+                        return;
+                    }
+                    ans = Math.max(ans, dist[h][i][j]);
+                }
+            }
+        }
+        System.out.println(ans);
     }
 
     static class FastReader {
