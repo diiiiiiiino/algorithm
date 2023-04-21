681af62f3acd37ee516274eff8ac28cd09bc6873
diiiiiiiino <eodyddnjs@naver.com>
2023-04-21 16:10:56 +0900

    이분탐색 문제풀이

---
+++ src/코딩테스트/백준/이분탐색/다시풀기/두수의합/Main.java
@@ -0,0 +1,106 @@
+package 코딩테스트.백준.이분탐색.다시풀기.두수의합;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/
+ */
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, x, ans;
+    static int[] a;
+
+    static void input(){
+        n = scan.nextInt();
+        a = new int[n + 1];
+
+        for(int i = 1; i <= n; i++){
+            a[i] = scan.nextInt();
+        }
+
+        x = scan.nextInt();
+        Arrays.sort(a, 1, n + 1);
+    }
+
+    static void binarySearch(int start, int num){
+        int L = start, R = n;
+
+        while(L <= R){
+            int mid = (L + R) / 2;
+
+            if(num + a[mid] == x){
+                ans++;
+                break;
+            }
+
+            if(num + a[mid] < x){
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+    }
+
+    static void pro(){
+        for(int i = 1; i <= n; i++){
+            binarySearch(i + 1, a[i]);
+        }
+    }
+
+    public static void main(String[] args){
+        input();
+        pro();
+        System.out.println(ans);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
