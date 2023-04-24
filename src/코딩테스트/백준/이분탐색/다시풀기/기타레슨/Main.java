407a278189688cdc24aa7cc09b60f05cde524b75
diiiiiiiino <eodyddnjs@naver.com>
2023-04-24 18:29:50 +0900

    기타레슨 문제풀이

--- src/코딩테스트/백준/이분탐색/다시풀기/기타레슨/Main.java
+++ src/코딩테스트/백준/이분탐색/다시풀기/기타레슨/Main.java
@@ -1,62 +1,70 @@
 package 코딩테스트.백준.이분탐색.다시풀기.기타레슨;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
- * https://www.acmicpc.net/problem/12738 다시풀기
+ * https://www.acmicpc.net/problem/2343
  */
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N;
-    static int[] nums;
-    static List<Integer> list = new ArrayList<>();
+    static int N, M;
+    static int[] a;
 
-    static void input() {
+
+    static void input(){
         N = scan.nextInt();
-        nums = new int[N + 1];
+        M = scan.nextInt();
+
+        a = new int[N + 1];
         for(int i = 1; i <= N; i++){
-            nums[i] = scan.nextInt();
+            a[i] = scan.nextInt();
         }
     }
 
-    static void func(){
-        list.add(Integer.MIN_VALUE);
-
+    static boolean determine(int mid){
+        int count = 1;
+        int sum = 0;
         for(int i = 1; i <= N; i++){
-            int val = nums[i];
-
-            if(val > list.get(list.size() - 1)){
-                list.add(val);
-            }else {
-                int L = 1, R = list.size() - 1;
-                while(L < R){
-                    int mid = (L+R) / 2;
-                    if(list.get(mid) >= val){
-                        R = mid;
-                    } else {
-                        L = mid + 1;
-                    }
-                }
-                list.set(R, val);
+            if(sum + a[i] > mid){
+                count++;
+                sum = a[i];
+            } else {
+                sum += a[i];
             }
         }
-        System.out.println(list.size() - 1);
+
+        return count <= M;
     }
 
-    public static void main(String[] args) {
+    static void pro(){
+        int L = a[1], R = 1000000000, ans = 0;
+        for (int i = 1; i <= N; i++) L = Math.max(L, a[i]);
+
+        while(L <= R){
+            int mid = (L + R) / 2;
+            if(determine(mid)){
+                R = mid - 1;
+                ans = mid;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args){
         input();
-        func();
+        pro();
     }
-
 
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
+
 
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
