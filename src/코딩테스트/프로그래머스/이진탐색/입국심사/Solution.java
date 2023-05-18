9880adcdfec85c47f7987e02b00a7c589e8b09fa
NohEunSeok <eodyddnjs@naver.com>
2023-05-18 21:11:10 +0900

    * 입국심사 / 평행 문제풀이

---
+++ src/코딩테스트/프로그래머스/이진탐색/입국심사/Solution.java
@@ -0,0 +1,35 @@
+package 코딩테스트.프로그래머스.이진탐색.입국심사;
+
+public class Solution {
+    private boolean isValid(long t, int n, int[] times){
+        long c = 0;
+        for(int time : times){
+            c += t / time;
+        }
+
+        return c >= n;
+    }
+
+    public long solution(int n, int[] times){
+        long start = 1;
+        long end = 1000000000000000000L;
+
+        while(end > start){
+            long t = (start + end) / 2;
+
+            if(isValid(t, n, times)){
+                end = t;
+            } else {
+                start = t + 1;
+            }
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        long result = s.solution(6, new int[]{7, 10});
+        System.out.println(result);
+    }
+}
