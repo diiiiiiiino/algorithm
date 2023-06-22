c9e981e8ce4779abf088c299095737872b32044a
NohEunSeok <eodyddnjs@naver.com>
2023-06-22 22:48:30 +0900

    * 모의고사

---
+++ src/코딩테스트/프로그래머스/완전탐색/모의고사/Solution.java
@@ -0,0 +1,39 @@
+package 코딩테스트.프로그래머스.완전탐색.모의고사;
+
+import java.util.*;
+
+class Solution {
+    private final int[][] b = { { 1, 2, 3, 4, 5 }, { 2, 1, 2, 3, 2, 4, 2, 5 }, { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+    
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        
+        int max = Integer.MIN_VALUE;
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < b.length; i++){
+            int[] a = b[i];
+            
+            int count = 0;
+            for(int j = 0; j < answers.length; j++){
+                if(answers[j] == a[j % a.length]){
+                    count++;
+                }
+            }
+            
+            if(max < count){
+                max = count;
+                arr.clear();
+                arr.add(i + 1);
+            } else if(max == count){
+                arr.add(i + 1);
+            }
+        }
+        
+        answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
