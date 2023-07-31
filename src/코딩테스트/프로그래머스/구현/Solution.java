ff1506d5d082df3f163faae74565237abf51583c
NohEunSeok <eodyddnjs@naver.com>
2023-07-31 22:08:28 +0900

    * 구현

---
+++ src/코딩테스트/프로그래머스/구현/Solution.java
@@ -0,0 +1,12 @@
+package 코딩테스트.프로그래머스.구현;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(String skill, String[] skillTrees){
+        return (int) Arrays.stream(skillTrees)
+                .map(s -> s.replaceAll("[^" + skill + "]", ""))
+                .filter(skill::startsWith)
+                .count();
+    }
+}
